648e8d3cc2c5fb9f69b93083eb8ea07b3f6267d8
SergiiGrynevych <grinevich.sergey.official@gmail.com>
2020-09-12 10:02:30 +0300

    1

--- js/other_practice.js
+++ js/other_practice.js
@@ -1,7 +1,21 @@
-const repeatLog = function (n) {
-  for (let i = 0; i < n; i += 1) {
-    console.log(i);
-  }
+// const repeatLog = function (n) {
+//   for (let i = 0; i < n; i += 1) {
+//     console.log(i);
+//   }
+// };
+
+// repeatLog(Infinity);
+
+const makeShef = function (name) {
+  /*
+   * Параметр name это локальная переменная для функции makeShef.
+   * Это значит что она будет доступна функции makeDish через замыкание.
+   */
+  return function makeDish(dish) {
+    console.log(`${name} is cooking ${dish}`);
+  };
 };
 
-repeatLog(Infinity);
+const mango = makeShef("Mango");
+mango("apple pie"); // Mango is cooking apple pie
+mango("beef stew"); // Mango is cooking beef stew
